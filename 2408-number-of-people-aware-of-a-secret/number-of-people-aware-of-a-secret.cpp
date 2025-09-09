023ec5d5fc181ca93012f45c692877af547c70c7
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2025-09-09 18:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (84.16%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
-       const int MOD = 1e9 + 7;
-        vector<long long> dp(n + 1, 0); 
+        const int MOD = 1e9 + 7;
+        vector<long long> dp(n + 1, 0);
         dp[1] = 1;
+        long long share = 0; 
 
-        for (int day = 1; day <= n; day++) {
-            long long people = dp[day] % MOD;
-
-            for (int next = day + delay; next < day + forget && next <= n; next++) {
-                dp[next] = (dp[next] + people) % MOD;
-            }
+        for (int day = 2; day <= n; day++) {
+            if (day - delay >= 1) share = (share + dp[day - delay]) % MOD;
+            if (day - forget >= 1) share = (share - dp[day - forget] + MOD) % MOD;
+            dp[day] = share;
         }
 
         long long ans = 0;
         for (int i = n - forget + 1; i <= n; i++) {
             if (i >= 1) ans = (ans + dp[i]) % MOD;
         }
-        return ans; 
+        return ans;
     }
 };
