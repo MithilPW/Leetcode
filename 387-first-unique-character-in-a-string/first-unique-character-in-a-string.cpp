f9b71d05fa2470e2e4dda581ead733a86de8d132
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2025-10-01 10:43:13 +0530

    Time: 17 ms (34.65%) | Memory: 20.4 MB (5.10%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        queue<int> q;   // store indices instead of chars
-        int freq[26] = {0};
+    queue<int> q;
+    int freq[26] = {0};
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-            freq[ch - 'a']++;
-            q.push(i);   // push index instead of character
+    for(int i=0;i<s.length();i++){
+        char ch = s[i];
+        freq[ch-'a']++;
+        q.push(i);
 
-            // Remove indices of characters that are no longer unique
-            while (!q.empty() && freq[s[q.front()] - 'a'] > 1) {
+
+        while (!q.empty() && freq[s[q.front()] - 'a'] > 1) {
                 q.pop();
-            }
         }
-
-        // If queue is empty, no unique char exists
-        if (q.empty()) return -1;
-
-        // Otherwise, front index is the first unique character
-        return q.front();
+    }
+    if(q.empty()){
+            return -1;
+        }
+    return q.front();
     }
 };
