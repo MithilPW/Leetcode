dca9f951504b9a98b73755c06e4e5ffda02eb4b2
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2025-10-03 01:17:59 +0530

    Time: 21 ms (67.16%) | Memory: 139.3 MB (34.19%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,27 +2,58 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq; 
-        vector<int> result;
-        int n = nums.size();
+        vector<int> ans;
 
-        for(int index = 0; index < n; index++) {
+        //first window ko process karo
 
-            if(!dq.empty() && dq.front() <= index - k) {
-                dq.pop_front();
-            }
+        for(int i=0;i<k;i++){
+            int element = nums[i];
 
-            while(!dq.empty() && nums[dq.back()] <= nums[index]) {
+            while(!dq.empty() && element > nums[dq.back()]){
                 dq.pop_back();
             }
 
-            dq.push_back(index);
-
-
-            if(index >= k - 1) {
-                result.push_back(nums[dq.front()]);
-            }
+            dq.push_back(i);
         }
 
-        return result;
+        for(int i=k;i<nums.size();i++){
+            ans.push_back(nums[dq.front()]);
+
+            if(!dq.empty() && i - dq.front() >= k){
+                dq.pop_front();
+            }
+            int element = nums[i];
+            while(!dq.empty() && element > nums[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        ans.push_back(nums[dq.front()]);
+
+        return ans;
+
+
+        // int n = nums.size();
+
+        // for(int index = 0; index < n; index++) {
+
+        //     if(!dq.empty() && dq.front() <= index - k) {
+        //         dq.pop_front();
+        //     }
+
+        //     while(!dq.empty() && nums[dq.back()] <= nums[index]) {
+        //         dq.pop_back();
+        //     }
+
+        //     dq.push_back(index);
+
+
+        //     if(index >= k - 1) {
+        //         result.push_back(nums[dq.front()]);
+        //     }
+        // }
+
+        // return result;
     }
 };
