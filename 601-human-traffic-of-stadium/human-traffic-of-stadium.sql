7b76ecc369c4ea0961c554ace2ec23ee767f8b3d
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2026-01-01 12:36:40 +0530

    Time: 342 ms (69.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+WITH filtered AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+),
+valid_groups AS (
+    SELECT grp
+    FROM filtered
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+SELECT id, visit_date, people
+FROM filtered
+WHERE grp IN (SELECT grp FROM valid_groups)
+ORDER BY visit_date;
