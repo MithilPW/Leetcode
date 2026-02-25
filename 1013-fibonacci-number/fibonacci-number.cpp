4a52f07024cab44f7fb7f76aad640eed8e9eedd7
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2026-02-25 16:43:01 +0530

    Time: 3 ms (65.09%) | Memory: 8.1 MB (18.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -8,12 +8,13 @@ public:
         if(dp[n] != -1){
             return dp[n];
         }
-        int ans = fib(n-1) + fib(n-2);
+        int ans = solve(n-1,dp) + solve(n-2,dp);
         dp[n] = ans;
         return dp[n];
     }
     int fib(int n) {
-        vector <int> dp(n+1,-1);
-        return solve(n,dp);
+        vector <int> dp(n+100,-1);
+        int ans = solve(n,dp);
+        return ans;
     }
 };
