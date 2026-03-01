6887d2824e3ee07af2ef057356df4ccc4f5f827d
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2026-03-01 20:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -30,12 +30,35 @@ public:
         return ans;
     }
 
+    //Tabulation / Bottom -Up apporach
+    int solveUsingTabulation(vector<int> &nums){
+        int n = nums.size();
+        vector <int> dp(n+1,-1);
+
+        dp[n] = 0;
+
+        //reverse the flow loop copy paste
+
+        for(int i = n-1;i>=0;i--){
+            int temp = 0;
+            if(i+2 <= n){
+                 temp = dp[i+2];
+            }
+            
+            int include = nums[i] + temp;
+            int exclude = 0 + dp[i+1];
+            dp[i] = max(include,exclude);
+        }
+
+        return dp[0];
+    }
+
     int rob(vector<int>& nums) {
         int index = 0;
         int n = nums.size();
         // int ans = solveUsingRec(nums,index);
         vector <int> dp(n+1,-1);
-        int ans = solveUsingMem(nums,index,dp);
+        int ans = solveUsingTabulation(nums);
         return ans;
     }
 };
