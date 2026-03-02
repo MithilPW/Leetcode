ad9613bce8658cfc4d443bf4bfe63254349fde72
Mithil Wadhai <157047482+MithilPW@users.noreply.github.com>
2026-03-02 20:14:47 +0530

    Time: 285 ms (30.05%) | Memory: 296.6 MB (12.56%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -40,14 +40,36 @@ public:
         dp[curr][prev+1] = length;
         return length;
     }
+
+    int solveusingTabulation(vector<int>& arr){
+        //create dp array
+        int n = arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        //base case and fill dp array
+
+        for(int curr = n-1;curr>=0;curr--){
+            for(int prev = curr-1;prev>=-1;prev--){
+                int include = 0;
+                if(prev == -1 || arr[curr] > arr[prev]){
+                    include = 1 + dp[curr+1][curr+1];
+                }
+                int exclude = 0 + dp[curr+1][prev+1];
+
+                dp[curr][prev+1] = max(include,exclude);
+            }
+            
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
         int prev = -1;
         int curr = 0;
 
-        int n =nums.size();
+        // int n =nums.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans = solveusingMemo(nums,curr,prev,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans = solveusingTabulation(nums);
 
         return ans;
     }
